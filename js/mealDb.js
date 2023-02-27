771d5891488b660198c0464c8ffb8465bf0e4f86
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2023-02-27 13:38:51 +0600

    material ui

--- js/mealDb.js
+++ js/mealDb.js
@@ -1,6 +1,6 @@
-console.log('working');
-const loadData = () => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=fish`)
+// console.log('working');
+const loadData = (id) => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${id}`)
         .then(res => res.json())
         .then(data => displayData(data.meals))
 }
@@ -8,21 +8,41 @@ const displayData = info => {
     info.forEach(element => {
         console.log(element)
         const mealContainer = document.getElementById('meal-container');
-        const mealDiv = document.createElement('section');
-        mealDiv.classList.add('col');
+        // mealContainer.innerText = '';
+        mealContainer.innerText = '';
+        const mealDiv = document.createElement('div');
+        mealDiv.classList.add('card')
         mealDiv.innerHTML = `
-       
-                <div class="card">
-                    <img src="${element.strMealThumb}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                            to additional content. This content is a little bit longer.</p>
-                    </div>
-                </div>
-        
+                <div class="mx-4 mt-4 translate-y-0 ">
+                            <a href="#" blur-shadow-image="true">
+                                <img class="w-auto rounded-lg"
+                                    src="${element.strMealThumb}"
+                                    alt="card image" />
+                            </a>
+                        </div>
+                        <div class="text-secondary flex-1 p-6">
+                            <span class="text-sm font-bold uppercase text-orange-500">${element.strMeal}</span>
+                            <a href="#">
+                                <h5 class="mt-2 font-medium text-blue-300">Dice-catagory: ${element.strCategory}</h5>
+                            </a>
+                            <p class="mb-3">
+                               ${element.strInstructions}
+                            </p>
+                          
+                            
+                        </div>
+                        
         `
         mealContainer.appendChild(mealDiv);
     });
 }
-loadData()
+
+const searchMeal = () => {
+
+    const searchFieldString = document.getElementById('search-field');
+    const searchField = searchFieldString.value;
+    console.log(searchField);
+    loadData(searchField);
+    searchFieldString.value = '';
+}
+loadData('rice');
